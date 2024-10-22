f6aea8553603d923e26bb97605bde83eaeb1c1f0
defaultname <>
2024-10-23 01:38:00 +0300

    natopomoika

---
+++ Natopomoika/scripts/skynet_red_iads.lua
@@ -0,0 +1,141 @@
+-- do
+-- --create an instance of the IADS
+-- redIADS = SkynetIADS:create('IRAN')
+
+-- ---debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
+-- local iadsDebug = redIADS:getDebugSettings()
+-- iadsDebug.IADSStatus = true
+-- iadsDebug.radarWentDark = true
+-- iadsDebug.contacts = true
+-- iadsDebug.radarWentLive = true
+-- iadsDebug.noWorkingCommmandCenter = false
+-- iadsDebug.ewRadarNoConnection = false
+-- iadsDebug.samNoConnection = false
+-- iadsDebug.jammerProbability = true
+-- iadsDebug.addedEWRadar = false
+-- iadsDebug.hasNoPower = false
+-- iadsDebug.harmDefence = true
+-- iadsDebug.samSiteStatusEnvOutput = true
+-- iadsDebug.earlyWarningRadarStatusEnvOutput = true
+-- iadsDebug.commandCenterStatusEnvOutput = true
+-- ---end remove debug ---
+
+-- --add all units with unit name beginning with 'EW' to the IADS:
+-- redIADS:addEarlyWarningRadarsByPrefix('EW')
+
+-- --add all groups begining with group name 'SAM' to the IADS:
+-- redIADS:addSAMSitesByPrefix('SAM')
+
+-- --add a command center:
+-- commandCenter = StaticObject.getByName('Command-Center')
+-- redIADS:addCommandCenter(commandCenter)
+
+-- ---we add a K-50 AWACs, manually. This could just as well be automated by adding an 'EW' prefix to the unit name:
+-- redIADS:addEarlyWarningRadar('AWACS-K-50')
+
+-- --add a power source and a connection node for this EW radar:
+-- local powerSource = StaticObject.getByName('Power-Source-EW-Center3')
+-- local connectionNodeEW = StaticObject.getByName('Connection-Node-EW-Center3')
+-- redIADS:getEarlyWarningRadarByUnitName('EW-Center3'):addPowerSource(powerSource):addConnectionNode(connectionNodeEW)
+
+-- --add a connection node to this SA-2 site, and set the option for it to go dark, if it looses connection to the IADS:
+-- local connectionNode = Unit.getByName('Mobile-Command-Post-SAM-SA-2')
+-- redIADS:getSAMSiteByGroupName('SAM-SA-2'):addConnectionNode(connectionNode):setAutonomousBehaviour(SkynetIADSAbstractRadarElement.AUTONOMOUS_STATE_DARK)
+
+-- --this SA-2 site will go live at 70% of its max search range:
+-- redIADS:getSAMSiteByGroupName('SAM-SA-2'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+-- --all SA-10 sites shall act as EW sites, meaning their radars will be on all the time:
+-- redIADS:getSAMSitesByNatoName('SA-10'):setActAsEW(true)
+
+-- --set the sa15 as point defence for the SA-10 site, we set it to always react to a HARM so we can demonstrate the point defence mechanism in Skynet
+-- local sa15 = redIADS:getSAMSiteByGroupName('SAM-SA-15-point-defence-SA-10')
+-- redIADS:getSAMSiteByGroupName('SAM-SA-10'):addPointDefence(sa15):setHARMDetectionChance(100)
+
+
+-- --set this SA-11 site to go live 70% of max range of its missiles (default value: 100%), its HARM detection probability is set to 50% (default value: 70%)
+-- redIADS:getSAMSiteByGroupName('SAM-SA-11'):setGoLiveRangeInPercent(70):setHARMDetectionChance(50)
+
+-- --this SA-6 site will always react to a HARM being fired at it:
+-- redIADS:getSAMSiteByGroupName('SAM-SA-6'):setHARMDetectionChance(100)
+
+-- --set this SA-11 site to go live at maximunm search range (default is at maximung firing range):
+-- redIADS:getSAMSiteByGroupName('SAM-SA-11-2'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE)
+
+-- --activate the radio menu to toggle IADS Status output
+-- redIADS:addRadioMenu()
+
+-- -- activate the IADS
+-- redIADS:activate()	
+
+-- --add the jammer
+-- local jammer = SkynetIADSJammer:create(Unit.getByName('jammer-emitter'), redIADS)
+-- jammer:masterArmOn()
+-- jammer:addRadioMenu()
+
+-- ---some special code to remove the jammer aircraft if player is not flying with it in formation, has nothing to do with the IADS:
+-- local hornet = Unit.getByName('Hornet SA-11-2 Attack')
+-- if hornet == nil then
+-- 	Unit.getByName('jammer-emitter'):destroy()
+-- 	jammer:removeRadioMenu()
+-- end
+-- --end special code
+
+-- ------setup blue IADS:
+-- blueIADS = SkynetIADS:create('UAE')
+-- blueIADS:addSAMSitesByPrefix('BLUE-SAM')
+-- blueIADS:addEarlyWarningRadarsByPrefix('BLUE-EW')
+-- blueIADS:activate()
+-- blueIADS:addRadioMenu()
+
+-- local iadsDebug = blueIADS:getDebugSettings()
+-- iadsDebug.IADSStatus = true
+-- iadsDebug.contacts = true
+
+-- end
+
+-- Red Hama IADS
+red_hama_iads = SkynetIADS:create('red_hama')
+red_hama_iads:addEarlyWarningRadarsByPrefix('EW_RED_HAMA')
+red_hama_iads:addSAMSitesByPrefix('SAM_RED_HAMA')
+red_hama_iads:addRadioMenu()
+red_hama_iads:activate()	
+
+-- Red Damascus IADS
+red_damascus_iads = SkynetIADS:create('red_damascus')
+red_damascus_iads:addEarlyWarningRadarsByPrefix('EW_RED_DAMASCUS')
+red_damascus_iads:addSAMSitesByPrefix('SAM_RED_DAMASCUS')
+red_damascus_iads:addRadioMenu()
+red_damascus_iads:activate()
+
+-- Red Palmyra IADS
+red_palmyra_iads = SkynetIADS:create('red_palmyra')
+red_palmyra_iads:addEarlyWarningRadarsByPrefix('EW_RED_PALMYRA')
+red_palmyra_iads:addSAMSitesByPrefix('SAM_RED_PALMYRA')
+red_palmyra_iads:addRadioMenu()
+red_palmyra_iads:activate()
+
+-- Red Tabqa IADS
+red_tabqa_iads = SkynetIADS:create('red_tabqa')
+red_tabqa_iads:addEarlyWarningRadarsByPrefix('EW_RED_TABQA')
+red_tabqa_iads:addSAMSitesByPrefix('SAM_RED_TABQA')
+red_tabqa_iads:addRadioMenu()
+red_tabqa_iads:activate()
+
+---debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
+local iadsDebug = red_hama_iads:getDebugSettings()
+iadsDebug.IADSStatus = true
+iadsDebug.radarWentDark = true
+iadsDebug.contacts = true
+iadsDebug.radarWentLive = true
+iadsDebug.noWorkingCommmandCenter = false
+iadsDebug.ewRadarNoConnection = false
+iadsDebug.samNoConnection = false
+iadsDebug.jammerProbability = true
+iadsDebug.addedEWRadar = false
+iadsDebug.hasNoPower = false
+iadsDebug.harmDefence = true
+iadsDebug.samSiteStatusEnvOutput = true
+iadsDebug.earlyWarningRadarStatusEnvOutput = true
+iadsDebug.commandCenterStatusEnvOutput = true
+---end remove debug ---
